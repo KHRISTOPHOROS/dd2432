dc6b1a228e2a066c35084fc29ae9525bf7d6cdc3
cnagy <cnagy@kth.se>
2013-01-21 01:59:04 +0100

    added lab1.py again?

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,15 +1,29 @@
 import numpy
+from numpy import *
 import pylab
+from pylab import * 
 import mlp
 import random
+f = open('class1','r')
+
+scale = double(raw_input("Scale for class2: "))
+center = double(raw_input("Center for class2: "))
+nhidden = int(raw_input("Number of hidden: "))
+beta = int(raw_input("Beta: "))
+momentum = double(raw_input("Momentum: "))
+eta = double(raw_input("Eta: "))
+iterations = int(raw_input("Iterations: "))
 
 #CREATE CLASSES
 class1 = numpy.random.randn(50,3)
-class1 = class1+3
-class1[:,2] = 0
+for i in range(len(class1)):
+    for j in range(3):
+        class1[i,j] = double(f.readline())
+
+print class1
 
 class2 = numpy.random.randn(50,3)
-class2 = class2-1
+class2 = class2*scale+center
 class2[:,2] = 1
 
 #FUSE CLASSES INTO ONE DATASET
@@ -36,27 +50,27 @@ indices = numpy.where(dataset[75:100,2]==1)
 testt[indices,0] = 1;
 
 #TRAINING!!!
-ANN = mlp.mlp(train,traint,1)
-ANN.mlptrain(train,traint,0.25,1)
+ANN = mlp.mlp(train,traint,nhidden,beta,momentum)
+ANN.mlptrain(train,traint,eta,iterations)
 ANN.confmat(train,traint)
 
 #PLOTTING?
-xRange = numpy.arange(-4,4,0.1)
-yRange = numpy.arange(-4,4,0.1)
-xgrid, ygrid = numpy.meshgrid(xRange,yRange)
-
-noOfPoints = xgrid.shape[0] * xgrid.shape[1]
-xcoords = xgrid.reshape((noOfPoints, 1))
-ycoords = ygrid.reshape((noOfPoints, 1))
-samples = numpy.concatenate((xcoords,ycoords), axis=1)
-
-ones = -numpy.ones(xcoords.shape)
-samples = numpy.concatenate((samples,ones), axis=1)
+xRange = numpy.arange(-4,4,0.1)                           ###
+yRange = numpy.arange(-4,4,0.1)                             #
+xgrid, ygrid = numpy.meshgrid(xRange,yRange)                #
+                                                            #
+noOfPoints = xgrid.shape[0] * xgrid.shape[1]                #
+xcoords = xgrid.reshape((noOfPoints, 1))                    ### CAN BE DONE WITH 2 FORLOOPS
+ycoords = ygrid.reshape((noOfPoints, 1))                    #
+samples = numpy.concatenate((xcoords,ycoords), axis=1)      #
+                                                            #
+ones = -numpy.ones(xcoords.shape)                           #
+samples = numpy.concatenate((samples,ones), axis=1)       ###
 
 indicator = ANN.mlpfwd(samples)
 indicator = indicator.reshape(xgrid.shape)
 
 pylab.contour(xRange,yRange,indicator, (0.5,))
+#pylab.plot(0, 1, 'o', 1, 0, 'o',1,1,'o',0,0,'o')
 pylab.plot(class1[:,0], class1[:,1], 'o',class2[:,0], class2[:,1],'o')
 pylab.show()
-
